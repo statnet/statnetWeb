c414eac5cda6f69b9892616e1c6e5da5c913613d
Emily Beylerian <ebey@uw.edu>
2014-07-14 11:40:48 -0700

    convert to navbar

--- ergm-common/ui.R
+++ ergm-common/ui.R
@@ -3,123 +3,130 @@
 library(shiny)
 library(statnet)
 
-shinyUI(fluidPage(
-  fluidRow(
-    column(2, img(src = 'csdelogo_crop.png', height = 75, width = 75)),
-    column(4, h4('Center for Studies in Demography and Ecology'))),
-  titlePanel('ERGM App for sample networks'),
-  p("This app is based on the ergm-mesa app, but will have more networks to choose from and more features."),
-  sidebarLayout(
-    sidebarPanel(
-      conditionalPanel(condition = "input.tabName == 'Plot Network'",
-                       h5('Choose a dataset'),
-                       selectInput('dataset',
-                                   label = 'Sample datasets',
-                                   c(Choose = '', 'ecoli1', 'ecoli2', 'faux.mesa.high','fauxhigh', 'flobusiness','flomarriage',
-                                     'kapferer','kapferer2','samplike'),
-                                   selectize = FALSE),
+shinyUI(navbarPage('ergm app',
+  tabPanel('Plot Network',
+   fluidRow(
+     column(1, img(src = 'csdelogo_crop.png', height = 50, width = 50)),
+     column(3, h6('Center for Studies in Demography and Ecology')),
+     column(8)),
+   
+    fluidRow(
+     column(4,
+      wellPanel(
+         h5('Choose a dataset'),
+         selectInput('dataset',
+                     label = 'Sample datasets',
+                     c(Choose = '', 'ecoli1', 'ecoli2', 'faux.mesa.high','fauxhigh', 'flobusiness','flomarriage',
+                       'kapferer','kapferer2','samplike'),
+                     selectize = FALSE),
                        br(),
-                       actionButton('goButton', 'Run')),
-      
-      conditionalPanel(condition = "input.tabName != 'Plot Network'",
-                       h5('Choose a dataset'),
-                       p('You are currently using the network', verbatimTextOutput('currentdataset'),
-                         'Return to the first tab if you would like to switch networks.')),
-      
-      conditionalPanel(condition = "input.tabName == 'Plot Network' | input.tabName == 'Simulations'",
-                       tags$hr(),
-                       h5('Display Options'),
-                       p('Pay attention to whether these options make sense for your network.'),
-                       checkboxInput('iso',
-                                     label = 'Display isolates?', 
-                                     value = TRUE),
-                       checkboxInput('vnames',
-                                     label = 'Display vertex names?',
-                                     value = FALSE),
-                       uiOutput('dynamiccolor'),
-                       uiOutput('dynamicsize'))
+                       actionButton('goButton', 'Run'),
+         tags$hr(),
+         h5('Display Options'),
+         p('Pay attention to whether these options make sense for your network.'),
+         checkboxInput('iso',
+                       label = 'Display isolates?', 
+                       value = TRUE),
+         checkboxInput('vnames',
+                       label = 'Display vertex names?',
+                       value = FALSE),
+         uiOutput('dynamiccolor'),
+         uiOutput('dynamicsize'))),
+                  
+      column(8, 
+             plotOutput('nwplot'),
+             h4('Network Summary'),
+             verbatimTextOutput('attr')))
+    ),
+                  
+      tabPanel('Fit Model',
+          fluidRow(
+            column(3,
+              wellPanel(
+                p('Current network:', verbatimTextOutput('currentdataset')),
+                p('Current ergm formula:',
+                  verbatimTextOutput('check1'))
+                )),
+          column(9,     
+               fluidRow(
+                 column(5,
+                        conditionalPanel(condition = 'input.dataset.gal.directed == TRUE',
+                                         uiOutput('dirlistofterms')),
+                        conditionalPanel(condition = 'input.dataset.gal.directed == FALSE',
+                                         uiOutput('undirlistofterms'))),
+                 column(4,
+                        conditionalPanel(condition = 'input.terms.indexOf("gwesp") > -1', 
+                                         numericInput('choosegwesp', label = 'pick alpha for gwesp',
+                                                      value = 0, min = 0),
+                                         checkboxInput('fixgwesp', label = 'fixed?', value = TRUE)),
+                        conditionalPanel(condition = 'input.terms.indexOf("degree") > -1',
+                                         uiOutput('dynamicdegree')),
+                        conditionalPanel(condition = 'input.terms.indexOf("nodematch") > -1',
+                                         uiOutput('dynamicnodematch')))
+               ),
+               fluidRow(
+                 column(3, p('Right hand side of ergm fit formula:')),
+                 column(5, verbatimTextOutput('check1'))
+                 ),
+               actionButton('fitButton', 'Fit Model')
+          )),
+         
+         br(),
+         tags$hr(),
+         p('The output of the model fitting process and the summary of the model
+           fit is below. Pay attention to the coefficient estimates and 
+           significance for each term.'),
+         br(),
+         p('Check for model degeneracy in the "Diagnostics" tab.'),
+         br(),
+         verbatimTextOutput('modelfit')     
+          
+          ),
+  
+          navbarMenu('Diagnostics',
+              tabPanel('Goodness of Fit',
+                       
+                       mainPanel(
+                       p('Test how well your model fits the original data by choosing 
+                         a network statistic that is not in the model, and comparing 
+                         the value of this statistic observed in the original network 
+                         to the distribution of values you get in simulated networks from 
+                         your model.'),
+                       p('If you do not specify a term the default formula for undirected 
+                         networks is ', code('~ degree + espartners + distance'), 'and for 
+                         directed networks is ', code('~ idegree + odegree + espartners + 
+                                                      distance'), '.'),
+                       fluidRow(
+                         column(6, selectInput('gofterm', 'Goodness of Fit Term:',
+                                               c(Default='', 'degree', 'distance', 'espartners', 
+                                                 'dspartners', 'triadcensus', 'model'),
+                                               selectize = FALSE)),
+                         column(4, actionButton('gofButton', 'Run'))),
+                       verbatimTextOutput('gof.summary'),  
+                       plotOutput('gofplot'))
                        ),
+              
+              tabPanel('MCMC Diagnostics',
+                       verbatimTextOutput('diagnostics'))
+              ),
+  
+          tabPanel('Simulations',
+                   fluidRow(
+                     column(6,
+                            numericInput('nsims',
+                                         label = 'Number of simulations:',
+                                         min = 1,
+                                         value = 1)),
+                     column(4,
+                            actionButton('simButton', 'Simulate'))),
+                   verbatimTextOutput('sim.summary'),
+                   numericInput('this.sim',
+                                label = 'Choose a simulation to plot',
+                                min = 1,
+                                value = 1),
+                   plotOutput('simplot'))
+                  
     
-    mainPanel(
-      tabsetPanel(type = 'tabs', id = 'tabName',
-                  tabPanel('Plot Network',
-                           plotOutput('nwplot'),
-                           h4('Network Summary'),
-                           verbatimTextOutput('attr')
-                  ),
-                  
-                  tabPanel('Fit Model',
-                           fluidRow(
-                             column(5,
-                                    conditionalPanel(condition = 'input.dataset.gal.directed == TRUE',
-                                                     uiOutput('dirlistofterms')),
-                                    conditionalPanel(condition = 'input.dataset.gal.directed == FALSE',
-                                                     uiOutput('undirlistofterms'))),
-                             column(4,
-                                    conditionalPanel(condition = 'input.terms.indexOf("gwesp") > -1', 
-                                                     numericInput('choosegwesp', label = 'pick alpha for gwesp',
-                                                                  value = 0, min = 0),
-                                                     checkboxInput('fixgwesp', label = 'fixed?', value = TRUE)),
-                                    conditionalPanel(condition = 'input.terms.indexOf("degree") > -1',
-                                                     uiOutput('dynamicdegree')),
-                                    conditionalPanel(condition = 'input.terms.indexOf("nodematch") > -1',
-                                                     uiOutput('dynamicnodematch')))
-                           ),
-                           actionButton('fitButton', 'Fit Model'),
-                           tags$hr(),
-                           fluidRow(
-                             p('Right hand side of ergm fit formula:'),
-                             verbatimTextOutput('check1'),
-                             p('The output of the model fitting process and the summary of the model
-                               fit is below. Pay attention to the coefficient estimates and 
-                               significance for each term.'),
-                             br(),
-                             p('Be sure to continue all the way to the "Goodness of Fit" and "Diagnostics"
-                               tabs to check for model degeneracy.'),
-                             br(),
-                             verbatimTextOutput('modelfit'))
-                             ),
-                  
-                  tabPanel('Goodness of Fit',
-                           p('Test how well your model fits the original data by choosing 
-                             a network statistic that is not in the model, and comparing 
-                             the value of this statistic observed in the original network 
-                             to the distribution of values you get in simulated networks from 
-                             your model.'),
-                           p('If you do not specify a term the default formula for undirected 
-                             networks is ', code('~ degree + espartners + distance'), 'and for 
-                             directed networks is ', code('~ idegree + odegree + espartners + 
-                                                          distance'), '.'),
-                           fluidRow(
-                             column(6, selectInput('gofterm', 'Goodness of Fit Term:',
-                                                   c(Default='', 'degree', 'distance', 'espartners', 
-                                                     'dspartners', 'triadcensus', 'model'),
-                                                   selectize = FALSE)),
-                             column(4, actionButton('gofButton', 'Run'))),
-                           verbatimTextOutput('gof.summary'),  
-                           plotOutput('gofplot')
-                           ),
-                  
-                  tabPanel('Diagnostics',
-                           verbatimTextOutput('diagnostics')),
-                  
-                  tabPanel('Simulations',
-                           fluidRow(
-                             column(6,
-                                    numericInput('nsims',
-                                                 label = 'Number of simulations:',
-                                                 min = 1,
-                                                 value = 1)),
-                             column(4,
-                                    actionButton('simButton', 'Simulate'))),
-                           verbatimTextOutput('sim.summary'),
-                           numericInput('this.sim',
-                                        label = 'Choose a simulation to plot',
-                                        min = 1,
-                                        value = 1),
-                           plotOutput('simplot'))
-                  )
-    )
     )
   )
-    )
+    
