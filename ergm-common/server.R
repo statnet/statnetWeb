75d267797f03bf7efc4be038b4a2d9eb2faed161
Emily Beylerian <ebey@uw.edu>
2014-07-16 08:56:59 -0700

    adding markdown and rearranging code chunks

--- ergm-common/server.R
+++ ergm-common/server.R
@@ -1,13 +1,15 @@
-#' ergm-common
+#' ergm-common, server.R
 #' ======================
 
+#+ eval=FALSE
+#make sure that both shiny and statnet packages are loaded
 library(shiny)
 library(statnet)
 
 shinyServer(
   function(input, output){
     
-    
+    #load datasets
     data(ecoli)
     data(florentine)
     data(fauxhigh)
@@ -15,6 +17,10 @@ shinyServer(
     data(kapferer)
     data(sampson)
     
+#' Saving the following vectors of terms will allow us to only display the terms
+#' that are applicable to a certain network. These don't depend on any user input
+#' and will never change value, so they don't need to be in a reactive expression.
+#+ eval=FALSE    
     dir.terms <- c('edges', 'nodefactor', 'nodematch', 'nodemix', 'nodecov',
                    'absdiff', 'gwesp', 'mutual', 'idegree', 'odegree')
     undir.terms <- c('edges', 'nodefactor', 'nodematch', 'nodemix', 'nodecov',
@@ -30,14 +36,20 @@ shinyServer(
 #' and they only get re-run when their values are outdated. Since many of 
 #' our render functions will be calling the same ergm objects, using 
 #' reactive expressions will help the app run much faster.
-    
+#'
+#' Notice that already in this chunk of code we call previously declared reactive
+#' objects. For example, to use the reactive list of vertex attributes in the
+#' definition of the numeric vertex attributes, we call `attr()`.    
+#+ eval=FALSE
     nw.reac <- reactive({eval(parse(text = input$dataset))})
-    nodes <- reactive({nw.reac()$gal$n}) #number of nodes in nw
+    #number of nodes in nw
+    nodes <- reactive({nw.reac()$gal$n}) 
+    #get coordinates to plot network with
     coords <- reactive({plot.network(eval(parse(text = input$dataset)))})
     
-    #list of attributes in nw
+    #list of vertex attributes in nw
     attr <- reactive({
-      attr <- ''
+      attr <- c()
       if(input$dataset != ''){      
         attr<-list.vertex.attributes(nw.reac())
       }
@@ -54,7 +66,10 @@ shinyServer(
         }} 
       numattr})
     
-    
+#' Some ergm terms (e.g. `gwesp`, `degree` and `nodematch`) take in their own arguments. 
+#' The following reactive expressions take user input and create vectors that can later
+#' be used as terms in an ergm formula.
+#+ eval=FALSE    
     gwesp.terms <- reactive({
       gterms <- paste("gwesp(",input$choosegwesp,
                       ", fixed = ",input$fixgwesp,")", sep="")
@@ -76,7 +91,11 @@ shinyServer(
         nterms <- NULL
       }
       nterms})
-    
+
+#' `ergm.terms` is a compilation of all the terms entered, without redundancies,
+#' which we then use to create a complete formula. 
+#' 
+#+ eval=FALSE    
     ergm.terms <- reactive({
       interms <- input$terms
       if (any(interms == 'gwesp')) {
@@ -90,19 +109,26 @@ shinyServer(
     
     ergm.formula <- reactive({
       formula(paste('nw.reac() ~ ',ergm.terms(), sep = ''))})
-    
+
+#' Once we have a formula, creating a model object, checking the goodness of fit
+#' and simulating from it is similar to what we would write in the command line,
+#' wrapped in a reactive statement.
+
+#+ eval=FALSE 
     model1.reac <- reactive({ergm(ergm.formula())})
     
+    #use default gof formula
+    model1.gof1 <- reactive({gof(model1.reac())})
+    
+    #use gof term that user specifies
+    gof.form <- reactive({
+      formula(paste('model1.reac() ~ ', input$gofterm, sep = ''))})
+    model1.gof2 <- reactive({gof(gof.form())})
+
     model1.sim.reac <- reactive({
       simulate(model1.reac(), nsim = input$nsims)})
     
-    model1.gof1 <- reactive({gof(model1.reac())})
-    
-    gof.form <- reactive({
-      formula(paste('model1.reac() ~ ', input$gofterm, sep = ''))})
-    
-    model1.gof2 <- reactive({gof(gof.form())})
-    
+    #get coordinates to plot simulations with
     sim.coords.1 <- reactive({plot.network(model1.sim.reac())})
     sim.coords.2 <- reactive({plot.network(model1.sim.reac()[[input$this.sim]])})
     
@@ -110,9 +136,65 @@ shinyServer(
 #' Output Expressions
 #' ---------------------------
 #' Every piece of content that gets displayed in the app has to be
-#' rendered by the appropriate `render*` function. Most of the render
-#' functions here call reactive objects that were created above.
-    #can't use single output twice?
+#' rendered by the appropriate `render*` function, e.g. `renderPrint` for text 
+#' and `renderPlot` for plots. Most of the render functions here call 
+#' reactive objects that were created above. I have divided the output objects
+#' into sections depending on what tab of the app they are called from.
+#'    
+#' **Plot Network**  
+#' 
+#' Because the menu options for coloring/sizing the nodes on a network plot 
+#' depend on which network has been selected, we have to dynamically render 
+#' these input menus, rather than statically defining them in `ui.R`.  
+#' including 
+#' 
+#+ eval=FALSE
+    output$dynamiccolor <- renderUI({
+      selectInput('colorby',
+                  label = 'Color nodes according to:',
+                  c('None' = 2, attr()),
+                  selectize = FALSE)
+    })
+    
+    output$dynamicsize <- renderUI({
+      selectInput('sizeby',
+                  label = 'Size nodes according to:',
+                  c('None' = 1, numattr()),
+                  selectize = FALSE)
+    })
+
+
+    output$nwplot <- renderPlot({
+      if (input$goButton == 0){
+        return()
+      }
+      input$goButton
+      nw <- isolate({nw.reac()})
+      #scale size of nodes onto range between .7 and 3.5
+      minsize <- min(get.vertex.attribute(nw,input$sizeby))
+      maxsize <- max(get.vertex.attribute(nw,input$sizeby))
+      if (input$sizeby == '1'){
+        size = 1
+      } else { 
+        size = (get.vertex.attribute(nw,input$sizeby)-minsize)/(maxsize-minsize)*(3.5-.7)+.7 
+      }
+      
+      plot.network(nw, coord = coords(), 
+                   displayisolates = input$iso, 
+                   displaylabels = input$vnames, 
+                   vertex.col = input$colorby,
+                   vertex.cex = size)
+    })
+
+
+    output$attr <- renderPrint({
+      if (input$goButton == 0){
+        return('Please choose a sample dataset from the side panel')
+      }
+      nw <- isolate(nw.reac())
+      return(nw)
+    })
+########################################
     output$check1 <- renderPrint({
       ergm.terms()
     })
@@ -138,19 +220,7 @@ shinyServer(
       input$dataset
     })
     
-    output$dynamiccolor <- renderUI({
-      selectInput('colorby',
-                  label = 'Color nodes according to:',
-                  c('None' = 2, attr()),
-                  selectize = FALSE)
-    })
     
-    output$dynamicsize <- renderUI({
-      selectInput('sizeby',
-                  label = 'Size nodes according to:',
-                  c('None' = 1, numattr()),
-                  selectize = FALSE)
-    })
     
     output$dynamicdegree <- renderUI({
       selectInput('choosedegree', 
@@ -168,37 +238,10 @@ shinyServer(
                   selectize = FALSE)
     })
     
-    output$nwplot <- renderPlot({
-      if (input$goButton == 0){
-        return()
-      }
-      input$goButton
-      nw <- isolate({nw.reac()})
-      #scale size of nodes onto range between .7 and 3.5
-      minsize <- min(get.vertex.attribute(nw,input$sizeby))
-      maxsize <- max(get.vertex.attribute(nw,input$sizeby))
-      if (input$sizeby == '1'){
-        size = 1
-      } else { 
-        size = (get.vertex.attribute(nw,input$sizeby)-minsize)/(maxsize-minsize)*(3.5-.7)+.7 
-      }
-      
-      
-      plot.network(nw, coord = coords(), 
-                   displayisolates = input$iso, 
-                   displaylabels = input$vnames, 
-                   vertex.col = input$colorby,
-                   vertex.cex = size)
-    })
     
     
-    output$attr <- renderPrint({
-      if (input$goButton == 0){
-        return('Please choose a sample dataset from the side panel')
-      }
-      nw <- isolate(nw.reac())
-      return(nw)
-    })
+    
+
 
 
     output$listofterms <- renderUI({
@@ -329,6 +372,5 @@ shinyServer(
         mcmc.diagnostics(model1)
       }
     })
-    
     
   })
