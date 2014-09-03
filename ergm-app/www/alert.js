d01352720d214f34bad8b274dad031e5ce40a793
Emily Beylerian <ebey@uw.edu>
2014-09-03 11:52:06 -0700

    change speed of hiding/showing boxes

--- ergm-app/www/alert.js
+++ ergm-app/www/alert.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
   $(".helper-btn").click(function(){
-    $(".helper-box").toggle();
+    $(".helper-box").toggle(500);
   });
 });
 
 $(document).ready(function(){
   $(".mixmxtitle").click(function(){
-    $(".mixmxbox").toggle();
+    $(".mixmxbox").toggle(1000);
   });
 });
 
 $(document).ready(function(){
   $(".nodeleveltitle").click(function(){
-    $(".nodelevelbox").toggle();
+    $(".nodelevelbox").toggle(1000);
   });
 });
 
 $(document).ready(function(){
   $(".graphleveltitle").click(function(){
-    $(".graphlevelbox").toggle();
+    $(".graphlevelbox").toggle(1000);
   });
 });
