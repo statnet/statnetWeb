c9e62b126c8277c6b937a4608f8b82d524ce9e52
EmilyB <ebey@uw.edu>
2014-09-15 15:23:21 -0700

    simplify alert.js

--- ergm-app/www/alert.js
+++ ergm-app/www/alert.js
@@ -2,25 +2,23 @@ $(document).ready(function(){
   $(".helper-btn").click(function(){
     $(".helper-box").toggle(500);
   });
-});
-
-$(document).ready(function(){
+  
   $(".mixmxtitle").click(function(){
     $(".mixmxbox").toggle(1000);
   });
-});
-
-$(document).ready(function(){
+  
   $(".nodeleveltitle").click(function(){
     $(".nodelevelbox").toggle(1000);
   });
-});
 
-$(document).ready(function(){
   $(".graphleveltitle").click(function(){
     $(".graphlevelbox").toggle(1000);
   });
+
+
 });
+
+
 /*
 window.onload = function(){ 
   //Get submit button
