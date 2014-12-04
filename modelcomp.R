cde9473c9e6f55f2dce0e4b6d0755c98aec240b9
EmilyB <ebey@uw.edu>
2014-12-03 17:58:54 -0800

    model.comparison function

--- modelcomp.R
+++ modelcomp.R
@@ -9,7 +9,7 @@ ergminfo <- function(object) {
   signif.stars <- symnum(pval, corr = FALSE, na = FALSE, 
                          cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1), 
                          symbols = c("***", "**", "*", ".", " "), legend=F)
-  starredcoef <- paste0(coefs, signif.stars)
+  starredcoef <- paste0(format(coefs, digits=3), signif.stars)
   
   ans <- list()
   count <-1
@@ -17,8 +17,8 @@ ergminfo <- function(object) {
     ans[i] <- starredcoef[count]
     count <- count + 1
   }
-  ans$aic <- summary(object)$aic
-  ans$bic <- summary(object)$bic
+  ans$AIC <- format(summary(object)$aic, digits=3)
+  ans$BIC <- format(summary(object)$bic, digits=3)
   ans
 }
 
@@ -29,9 +29,26 @@ model.comparison <- function(listobj) {
   n <- length(listobj)
   if(n>5) {stop("List of length greater than 5 passed to model.comparison")}
   allterms <- c()
-  for(j in n){
+  for(j in 1:n){
     allterms <- append(allterms, names(listobj[[j]]))
   }
   allterms <- sort(unique(allterms))
+  allterms <- c(allterms[-which(allterms=="AIC")],"AIC")
+  allterms <- c(allterms[-which(allterms=="BIC")],"BIC")
+  mat <- matrix(nrow=length(allterms),ncol=n)
+  for(k in 1:n){
+    row <- 1
+    for(l in allterms){
+      if(is.null(listobj[[k]][[l]])){
+        mat[row,k] <- "NA"
+      } else {
+        mat[row,k] <- listobj[[k]][[l]]
+      }
+      row <- row+1
+    }
+  }
   
+  rownames(mat) <- allterms
+  colnames(mat) <- paste0("Model",1:n )
+  return(print(mat, quote=F))
 }
