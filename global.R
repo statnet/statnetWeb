44cfbcea8b6ef24fd1e6ddbea169b8f760e7dc24
Emily B <ebey@uw.edu>
2014-12-08 14:39:43 -0800

    put custom widgets in global.R

---
+++ global.R
@@ -0,0 +1,47 @@
+
+# version of textInput with more size options
+# specify class = "input-small" or class="input-mini" in addition to other textInput args
+customTextInput<-function (inputId, label, value="",...) {
+  tagList(
+    tags$label(label, `for` = inputId), 
+    tags$input(id = inputId, type="text", value=value,...))
+}
+
+customNumericInput<-function (inputId, label, value=0, labelstyle="display:inline;", ...) {
+  tagList(
+    tags$label(label, `for` = inputId, style=labelstyle), 
+    tags$input(id = inputId, type="number", value=value,...))
+}
+
+#version of selectInput...shorter box and label inline
+#lapply allows us to add each element of choices as an option in the select menu
+inlineSelectInput<-function (inputId, label, choices,...) {
+  tagList(
+    tags$label(label, `for` = inputId, style="display:inline"), 
+    tags$select(id = inputId, choices=choices,..., 
+                style="width:100px; line-height:20px; font-size:12px",
+                class="shiny-bound-input",
+                lapply(choices, tags$option)))
+}
+
+#create a list of unique term names
+# UNCOMMENT AFTER STATNET RELEASE FOR TERM DOCUMENTATION
+# allterms <- search.ergmTerms()
+# inds <- gregexpr(pattern='\\(', allterms)
+# for(i in 1:length(allterms)){
+#   allterms[i] <- substr(allterms[[i]], start=1, stop=inds[[i]][1]-1)
+# }
+# allterms <- unique(allterms)
+
+#disable widgets when they should not be usable
+disableWidget <- function(id, session, disabled=TRUE){
+  if(disabled){
+    session$sendCustomMessage(type="jsCode",
+                              list(code=paste("$('#",id,"').prop('disabled',true)",
+                                              sep="")))
+  } else {
+    session$sendCustomMessage(type="jsCode",
+                              list(code=paste("$('#",id,"').prop('disabled',false)",
+                                              sep="")))
+  }
+}
