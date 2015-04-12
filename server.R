c332af9d88f2b6b77813d2d1c906da6ad5b7b398
Emily Beylerian <ebey@uw.edu>
2015-04-12 15:53:47 -0700

    v0.3.3

--- server.R
+++ server.R
@@ -4,7 +4,7 @@
 #' ---
 #' statnetWeb
 #' ===========
-#' server.R, v0.3.2 
+#' server.R, v0.3.3 
 #' ===========
 
 #' **Before reading this document:** The Shiny app "statnetWeb" is not contained in a
