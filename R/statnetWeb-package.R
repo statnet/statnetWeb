e6ba0c38e4f4d77c3d9d817dc5f99c7d764e75c3
Emily Beylerian <ebey@uw.edu>
2015-06-16 16:01:49 -0700

    import selectively from sna

--- R/statnetWeb-package.R
+++ R/statnetWeb-package.R
@@ -8,6 +8,18 @@
 #' @import shiny
 #' @import ergm
 #' @import network
-#' @import sna
+#' @importFrom sna betweenness
+#' @importFrom sna centralization
+#' @importFrom sna closeness
+#' @importFrom sna degree
+#' @importFrom sna evcent
+#' @importFrom sna gden
+#' @importFrom sna geodist
+#' @importFrom sna graphcent
+#' @importFrom sna grecip
+#' @importFrom sna gtrans
+#' @importFrom sna infocent
+#' @importFrom sna rgraph
+#' @importFrom sna symmetrize
 #' @importFrom RColorBrewer brewer.pal
 NULL
