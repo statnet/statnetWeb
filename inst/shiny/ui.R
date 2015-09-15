c4e26ae2d8073e6145bb4622f857eacb4a4ef640
Emily Beylerian <ebey@uw.edu>
2015-09-15 15:31:05 -0700

    add option for density plots (#51 and #58)

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -505,9 +505,15 @@ fluidRow(
                                 dataTableOutput("attrtbl")
                                 ),
                conditionalPanel('input.attrview == "histogram"',
+                                tags$label("Type of plots"),
+                                helpText("Density plots will only be created for",
+                                         "numeric attributes with more than nine",
+                                         "levels."),
                                 selectInput("attrhistaxis",
-                                            label = "Y-axis units",
-                                            choices = c("Counts", "Percents")),
+                                            label = NULL,
+                                            choices = c("Barplot: counts" = "count",
+                                                        "Barplot: percents" = "percent",
+                                                        "Density plot" = "density")),
                                 uiOutput("attrhistplotspace"))
 
                ),
