0139850b19c9a25189b8143601d682bcf77adb6f
Emily Beylerian <ebey@uw.edu>
2015-11-22 11:42:48 -0800

    comment out more

--- inst/shiny/stergm/server.R
+++ inst/shiny/stergm/server.R
@@ -28,17 +28,17 @@ values <- reactiveValues()
 
 # when two options are available to the user, or when we need to know if one
 # variable is outdated this reactive value will keep track of the state
-state <- reactiveValues(symmdir = FALSE, plotperc_dd = FALSE,
-                        plotperc_gd = FALSE, allterms = FALSE, gof = 0)
-
-# To keep a list of all attributes uploaded by the user:
-values$v_attrNamesToAdd <- list(1)
-values$v_attrValsToAdd <- list()
-values$e_attrNamesToAdd <- list(1)
-values$e_attrValsToAdd <- list()
-values$ev_attrNamesToAdd <- list(1)
-values$ev_attrValsToAdd <- list()
-values$input_termslist <- list()
+# state <- reactiveValues(symmdir = FALSE, plotperc_dd = FALSE,
+#                         plotperc_gd = FALSE, allterms = FALSE, gof = 0)
+#
+# # To keep a list of all attributes uploaded by the user:
+# values$v_attrNamesToAdd <- list(1)
+# values$v_attrValsToAdd <- list()
+# values$e_attrNamesToAdd <- list(1)
+# values$e_attrValsToAdd <- list()
+# values$ev_attrNamesToAdd <- list(1)
+# values$ev_attrValsToAdd <- list()
+# values$input_termslist <- list()
 
 values$nwnum <- "single"
 
@@ -164,7 +164,8 @@ nwinit <- reactive({
                                  network.list = nw_var,
                                  onsets = seq(from=0,length=length(nw_var)),
                                  termini = seq(from=1,length=length(nw_var)),
-                                 verbose = FALSE)
+                                 verbose = FALSE,
+                                 create.TEAs = FALSE) # change to TRUE if attributes change through time
       }
       if("network" %in% class(nw_var)){
         if(!is.element('bipartite', names(nw_var$gal))){
@@ -196,38 +197,38 @@ nwname <- reactive({
   name
 })
 
-#number of nodes in nw
-nodes <- reactive({
-  if(!is.network(nwinit())){return()}
-  nwinit()$gal$n
-})
-
-#number of edges in initial nw
-nedgesinit <- reactive({
-  if (is.network(nwinit())){
-    network.edgecount(nwinit())
-  } else if (class(nwinit()) == "list"){
-    network.edgecount(nwinit()[[1]])
-  }
-})
-
-#initial vertex attributes
-vattrinit <- reactive({
-  vattrinit <- c()
-  if(is.network(nwinit())){
-    vattrinit<-list.vertex.attributes(nwinit())
-  }
-  vattrinit
-})
-
-#matrix of vertex attribute values
-vattrinit.vals <- reactive({
-  v <- list()
-  for (j in seq(length(list.vertex.attributes(nwinit())))) {
-    v[[j]] <- get.vertex.attribute(nwinit(), vattrinit()[j])
-  }
-  v
-})
+# #number of nodes in nw
+# nodes <- reactive({
+#   if(!is.network(nwinit())){return()}
+#   nwinit()$gal$n
+# })
+#
+# #number of edges in initial nw
+# nedgesinit <- reactive({
+#   if (is.network(nwinit())){
+#     network.edgecount(nwinit())
+#   } else if (class(nwinit()) == "list"){
+#     network.edgecount(nwinit()[[1]])
+#   }
+# })
+#
+# #initial vertex attributes
+# vattrinit <- reactive({
+#   vattrinit <- c()
+#   if(is.network(nwinit())){
+#     vattrinit<-list.vertex.attributes(nwinit())
+#   }
+#   vattrinit
+# })
+#
+# #matrix of vertex attribute values
+# vattrinit.vals <- reactive({
+#   v <- list()
+#   for (j in seq(length(list.vertex.attributes(nwinit())))) {
+#     v[[j]] <- get.vertex.attribute(nwinit(), vattrinit()[j])
+#   }
+#   v
+# })
 
 #set correct number of rows for the attribute value lists,
 #so that we can add columns later
@@ -474,162 +475,162 @@ nw <- reactive({
   nw_var
 })
 
-elist <- reactive({
-  if(!is.network(nwinit())) return()
-  as.edgelist(nw())
-})
-
-#get coordinates to plot network with
-coords <- reactive({
-  input$refreshplot
-  plot.network(nw())
-})
-
-#initial network attributes
-#returns vector of true/falses
-nwattrinit <- reactive({
-  if(!is.network(nwinit())){return()}
-  nwattributes <- c('directed', 'hyper', 'loops', 'multiple', 'bipartite')
-  unlist(lapply(nwattributes, get.network.attribute, x = nwinit()))
-})
-
-#list of all vertex attributes in nw (after adding new)
-attrib <- reactive({
-  attr <- c()
-  if(is.network(nw())){
-    attr <- list.vertex.attributes(nw())
-  }
-  attr
-})
-
-#don't allow "na" or "vertex.names" as vertex attributes in menus on fit tab
-menuattr <- reactive({
-  menuattr <- attrib()
-  if(is.element("na", menuattr)){
-    menuattr <- menuattr[-which("na" == menuattr)]
-  }
-  if(is.element("vertex.names", menuattr)){
-    menuattr <- menuattr[-which("vertex.names" == menuattr)]
-  }
-  menuattr
-})
-
-#numeric attributes only (for size menu, etc.)
-numattr <- reactive({
-  numattr <- c()
-  if(is.network(nw())){
-    for(i in 1:length(attrib())){
-      if(is.numeric(get.vertex.attribute(nw(),attrib()[i]))){
-        numattr <- append(numattr,attrib()[i])
-      }
-    }}
-  numattr
-})
-
-#dataframe of nodes, their attributes, and their coordinates in nwplot
-nwdf <- reactive({
-  attrs <- menuattr()
-  if(is.na(as.numeric(network.vertex.names(nw()))[1])){
-    df <- data.frame(Names = network.vertex.names(nw()))
-  } else {
-    df <- data.frame(Names = as.numeric(network.vertex.names(nw())))
-  }
-  for(i in seq(length(attrs))){
-    df[[attrs[i]]] <- get.vertex.attribute(nw(), attrs[i])
-  }
-  df[["Missing"]] <- get.vertex.attribute(nw(), "na")
-  df[["cx"]] <- coords()[,1]
-  df[["cy"]] <- coords()[,2]
-  df
-})
-
-# betweenness centrality of all nodes (for sizing menu)
-nodebetw <- reactive({
-  if(!is.network(nw())){return()}
-  if(is.directed(nw())){
-    gmode <- 'digraph'
-    cmode <- 'directed'
-  } else {
-    gmode <- 'graph'
-    cmode <- 'undirected'
-  }
-  sna::betweenness(nw(), gmode = gmode, diag = has.loops(nw()),
-                   cmode = cmode)
-})
-
-nodesize <- reactive({
-  if(!is.network(nw())){return()}
-  nw_var <- nw()
-  #scale size of nodes onto range between .7 and 3.5
-  if (input$sizeby == '1'){
-    size = 1
-  } else if (input$sizeby == 'Betweenness'){
-    minsize <- min(nodebetw())
-    maxsize <- max(nodebetw())
-    size = (nodebetw()-minsize)/(maxsize-minsize)*(3.5-.7)+.7
-  } else {
-    minsize <- min(get.vertex.attribute(nw_var,input$sizeby))
-    maxsize <- max(get.vertex.attribute(nw_var,input$sizeby))
-    size <- (get.vertex.attribute(nw_var,input$sizeby)-minsize) /
-      (maxsize-minsize) * (3.5 - .7) + .7
-  }
-  size})
-
-#vertex color
-vcol <- reactive({
-  if(!is.network(nw())){return()}
-  nw_var <- nw()
-  if(input$colorby == 2){
-    vcol <- rep(2, nodes())
-  } else {
-    full_list <- get.vertex.attribute(nw_var,input$colorby)
-    short_list <- sort(unique(full_list))
-    ncolors <- length(short_list)
-    if(is.element("Other", short_list)){ #to be consistent with order of legend
-      short_list <- short_list[-which(short_list == "Other")]
-      short_list <- c(short_list, "Other")
-    }
-    full_list <- match(full_list, short_list)
-    #each elt corresponds to integer position in short_list
-    pal <- c('red', 'blue', 'green3', 'cyan', 'magenta3',
-             'yellow', 'orange', 'black', 'grey')
-    if(ncolors>9){
-      pal <- colorRampPalette(brewer.pal(11,"RdYlBu"))(ncolors)
-    }
-    vcol <- pal[full_list]
-  }
-  vcol
-})
-
-legendlabels <- reactive({
-  if(!is.network(nw())){return()}
-  nw_var <- nw()
-  if(input$colorby == 2){
-    legendlabels <- NULL
-  }else{
-    legendlabels <- sort(unique(get.vertex.attribute(nw_var, input$colorby)))
-    if(is.element("Other", legendlabels)){
-      legendlabels <- legendlabels[-which(legendlabels == "Other")]
-      legendlabels <- c(legendlabels, "Other")
-    }
-  }
-  legendlabels
-})
-
-legendfill <- reactive({
-  if(input$colorby == 2){
-    legendfill <- NULL
-  } else {
-    n <- length(legendlabels())
-    pal <- c('red', 'blue', 'green3', 'cyan', 'magenta3',
-             'yellow', 'orange', 'black', 'grey')
-    if(n>9){
-      pal <- colorRampPalette(brewer.pal(11,"RdYlBu"))(n)
-    }
-    legendfill <- adjustcolor(pal, alpha.f = input$transp)
-  }
-  legendfill
-})
+# elist <- reactive({
+#   if(!is.network(nwinit())) return()
+#   as.edgelist(nw())
+# })
+#
+# #get coordinates to plot network with
+# coords <- reactive({
+#   input$refreshplot
+#   plot.network(nw())
+# })
+#
+# #initial network attributes
+# #returns vector of true/falses
+# nwattrinit <- reactive({
+#   if(!is.network(nwinit())){return()}
+#   nwattributes <- c('directed', 'hyper', 'loops', 'multiple', 'bipartite')
+#   unlist(lapply(nwattributes, get.network.attribute, x = nwinit()))
+# })
+#
+# #list of all vertex attributes in nw (after adding new)
+# attrib <- reactive({
+#   attr <- c()
+#   if(is.network(nw())){
+#     attr <- list.vertex.attributes(nw())
+#   }
+#   attr
+# })
+#
+# #don't allow "na" or "vertex.names" as vertex attributes in menus on fit tab
+# menuattr <- reactive({
+#   menuattr <- attrib()
+#   if(is.element("na", menuattr)){
+#     menuattr <- menuattr[-which("na" == menuattr)]
+#   }
+#   if(is.element("vertex.names", menuattr)){
+#     menuattr <- menuattr[-which("vertex.names" == menuattr)]
+#   }
+#   menuattr
+# })
+#
+# #numeric attributes only (for size menu, etc.)
+# numattr <- reactive({
+#   numattr <- c()
+#   if(is.network(nw())){
+#     for(i in 1:length(attrib())){
+#       if(is.numeric(get.vertex.attribute(nw(),attrib()[i]))){
+#         numattr <- append(numattr,attrib()[i])
+#       }
+#     }}
+#   numattr
+# })
+#
+# #dataframe of nodes, their attributes, and their coordinates in nwplot
+# nwdf <- reactive({
+#   attrs <- menuattr()
+#   if(is.na(as.numeric(network.vertex.names(nw()))[1])){
+#     df <- data.frame(Names = network.vertex.names(nw()))
+#   } else {
+#     df <- data.frame(Names = as.numeric(network.vertex.names(nw())))
+#   }
+#   for(i in seq(length(attrs))){
+#     df[[attrs[i]]] <- get.vertex.attribute(nw(), attrs[i])
+#   }
+#   df[["Missing"]] <- get.vertex.attribute(nw(), "na")
+#   df[["cx"]] <- coords()[,1]
+#   df[["cy"]] <- coords()[,2]
+#   df
+# })
+#
+# # betweenness centrality of all nodes (for sizing menu)
+# nodebetw <- reactive({
+#   if(!is.network(nw())){return()}
+#   if(is.directed(nw())){
+#     gmode <- 'digraph'
+#     cmode <- 'directed'
+#   } else {
+#     gmode <- 'graph'
+#     cmode <- 'undirected'
+#   }
+#   sna::betweenness(nw(), gmode = gmode, diag = has.loops(nw()),
+#                    cmode = cmode)
+# })
+#
+# nodesize <- reactive({
+#   if(!is.network(nw())){return()}
+#   nw_var <- nw()
+#   #scale size of nodes onto range between .7 and 3.5
+#   if (input$sizeby == '1'){
+#     size = 1
+#   } else if (input$sizeby == 'Betweenness'){
+#     minsize <- min(nodebetw())
+#     maxsize <- max(nodebetw())
+#     size = (nodebetw()-minsize)/(maxsize-minsize)*(3.5-.7)+.7
+#   } else {
+#     minsize <- min(get.vertex.attribute(nw_var,input$sizeby))
+#     maxsize <- max(get.vertex.attribute(nw_var,input$sizeby))
+#     size <- (get.vertex.attribute(nw_var,input$sizeby)-minsize) /
+#       (maxsize-minsize) * (3.5 - .7) + .7
+#   }
+#   size})
+#
+# #vertex color
+# vcol <- reactive({
+#   if(!is.network(nw())){return()}
+#   nw_var <- nw()
+#   if(input$colorby == 2){
+#     vcol <- rep(2, nodes())
+#   } else {
+#     full_list <- get.vertex.attribute(nw_var,input$colorby)
+#     short_list <- sort(unique(full_list))
+#     ncolors <- length(short_list)
+#     if(is.element("Other", short_list)){ #to be consistent with order of legend
+#       short_list <- short_list[-which(short_list == "Other")]
+#       short_list <- c(short_list, "Other")
+#     }
+#     full_list <- match(full_list, short_list)
+#     #each elt corresponds to integer position in short_list
+#     pal <- c('red', 'blue', 'green3', 'cyan', 'magenta3',
+#              'yellow', 'orange', 'black', 'grey')
+#     if(ncolors>9){
+#       pal <- colorRampPalette(brewer.pal(11,"RdYlBu"))(ncolors)
+#     }
+#     vcol <- pal[full_list]
+#   }
+#   vcol
+# })
+#
+# legendlabels <- reactive({
+#   if(!is.network(nw())){return()}
+#   nw_var <- nw()
+#   if(input$colorby == 2){
+#     legendlabels <- NULL
+#   }else{
+#     legendlabels <- sort(unique(get.vertex.attribute(nw_var, input$colorby)))
+#     if(is.element("Other", legendlabels)){
+#       legendlabels <- legendlabels[-which(legendlabels == "Other")]
+#       legendlabels <- c(legendlabels, "Other")
+#     }
+#   }
+#   legendlabels
+# })
+#
+# legendfill <- reactive({
+#   if(input$colorby == 2){
+#     legendfill <- NULL
+#   } else {
+#     n <- length(legendlabels())
+#     pal <- c('red', 'blue', 'green3', 'cyan', 'magenta3',
+#              'yellow', 'orange', 'black', 'grey')
+#     if(n>9){
+#       pal <- colorRampPalette(brewer.pal(11,"RdYlBu"))(n)
+#     }
+#     legendfill <- adjustcolor(pal, alpha.f = input$transp)
+#   }
+#   legendfill
+# })
 
 #simulated graphs for cug tests
 # observeEvent(c(nw(), input$ncugsims),{
@@ -652,98 +653,98 @@ legendfill <- reactive({
 #   }
 # })
 
-brgvals <- reactive({
-  apply(values$cugsims[[1]], MARGIN = 1, FUN = cugstats,
-        term = input$cugtestterm, directed = nw()$gal$directed,
-        loops = nw()$gal$loops)
-})
-cugvals <- reactive({
-  apply(values$cugsims[[2]], MARGIN = 1, FUN = cugstats,
-        term = input$cugtestterm, directed = nw()$gal$directed,
-        loops = nw()$gal$loops)
-})
+# brgvals <- reactive({
+#   apply(values$cugsims[[1]], MARGIN = 1, FUN = cugstats,
+#         term = input$cugtestterm, directed = nw()$gal$directed,
+#         loops = nw()$gal$loops)
+# })
+# cugvals <- reactive({
+#   apply(values$cugsims[[2]], MARGIN = 1, FUN = cugstats,
+#         term = input$cugtestterm, directed = nw()$gal$directed,
+#         loops = nw()$gal$loops)
+# })
 
 ## FIT MODEL ##
-
-formation <- reactive({
-  input$updateformulaButton
-  isolate({input$formation})
-})
-
-dissolution <- reactive({
-  paste(input$dissolution, collapse = " + ")
-})
-
-observeEvent(input$resetformulaButton, {
-  updateTextInput(session, "formation",
-                  label = NULL, value = "edges")
-})
+#
+# formation <- reactive({
+#   input$updateformulaButton
+#   isolate({input$formation})
+# })
+#
+# dissolution <- reactive({
+#   paste(input$dissolution, collapse = " + ")
+# })
+#
+# observeEvent(input$resetformulaButton, {
+#   updateTextInput(session, "formation",
+#                   label = NULL, value = "edges")
+# })
 # observeEvent(nw(), {
 #   updateTextInput(session, "formation",
 #                   label = NULL, value = "edges")
 # })
-
-dissoffsets <- reactive({
-  ncoefs <- length(input$dissolution)
-  ids <- paste0("coef", ncoefs)
-  unlist(input[ids])
-})
-
-estimate <- reactive({
-  if("networkDynamic" %in% class(nw())){
-    "CMLE"
-  } else {
-    "EGMME"
-  }
-})
-
-stergmcontrols <- reactive({
-  customcontrols <- isolate(paste(input$customMCMCcontrol, sep = ","))
-  if(customcontrols == ""){
-    if(estimate() == "EGMME"){
-      control.stergm(EGMME.MCMC.burnin.min = input$EGMME.burnin.min,
-                     EGMME.MCMC.burnin.max = input$EGMME.burnin.max,
-                     EGMME.MCMC.burnin.add = input$EGMME.burnin.add,
-                     EGMME.MCMC.burnin.pval = input$EGMME.burnin.pval)
-    }
-  } else {
-    if(estimate() == "EGMME"){
-      control.stergm(EGMME.MCMC.burnin.min = input$EGMME.burnin.min,
-                     EGMME.MCMC.burnin.max = input$EGMME.burnin.max,
-                     EGMME.MCMC.burnin.add = input$EGMME.burnin.add,
-                     EGMME.MCMC.burnin.pval = input$EGMME.burnin.pval,
-                     eval(parse(text = customcontrols)))
-    }
-  }
-})
-
-#stergm model object
-stergm.fit <- reactive({
-  if(input$fitButton == 0){
-    return()
-  }
-  usingdefault <- isolate(input$controldefault)
-  if(usingdefault){
-    isolate({
-      fit <- stergm(nw(),
-                    formation = as.formula(paste("~", formation())),
-                    dissolution = as.formula(paste("~", dissolution())),
-                    targets = "formation",
-                    offset.coef.diss = dissoffsets(),
-                    estimate = estimate())
-    })
-  } else {
-      fit <- stergm(nw(),
-                    formation = as.formula(paste("~", formation())),
-                    dissolution = as.formula(paste("~", dissolution())),
-                    targets = "formation",
-                    offset.coef.diss = dissoffsets(),
-                    estimate = estimate(),
-                    control = stergmcontrols())
-  }
-
-  return(fit)
-})
+#
+# dissoffsets <- reactive({
+#   ncoefs <- length(input$dissolution)
+#   ids <- paste0("coef", ncoefs)
+#   unlist(input[ids])
+# })
+#
+# estimate <- reactive({
+#   if("networkDynamic" %in% class(nw())){
+#     "CMLE"
+#   } else {
+#     "EGMME"
+#   }
+# })
+#
+# stergmcontrols <- reactive({
+#   customcontrols <- isolate(paste(input$customMCMCcontrol, sep = ","))
+#   if(customcontrols == ""){
+#     if(estimate() == "EGMME"){
+#       control.stergm(EGMME.MCMC.burnin.min = input$EGMME.burnin.min,
+#                      EGMME.MCMC.burnin.max = input$EGMME.burnin.max,
+#                      EGMME.MCMC.burnin.add = input$EGMME.burnin.add,
+#                      EGMME.MCMC.burnin.pval = input$EGMME.burnin.pval)
+#     }
+#   } else {
+#     if(estimate() == "EGMME"){
+#       control.stergm(EGMME.MCMC.burnin.min = input$EGMME.burnin.min,
+#                      EGMME.MCMC.burnin.max = input$EGMME.burnin.max,
+#                      EGMME.MCMC.burnin.add = input$EGMME.burnin.add,
+#                      EGMME.MCMC.burnin.pval = input$EGMME.burnin.pval,
+#                      eval(parse(text = customcontrols)))
+#     }
+#   }
+# })
+#
+# #stergm model object
+# stergm.fit <- reactive({
+#   if(input$fitButton == 0){
+#     return()
+#   }
+#   usingdefault <- isolate(input$controldefault)
+#   if(usingdefault){
+#     isolate({
+#       fit <- stergm(nw(),
+#                     formation = as.formula(paste("~", formation())),
+#                     dissolution = as.formula(paste("~", dissolution())),
+#                     targets = "formation",
+#                     offset.coef.diss = dissoffsets(),
+#                     estimate = estimate())
+#     })
+#   } else {
+#       fit <- stergm(nw(),
+#                     formation = as.formula(paste("~", formation())),
+#                     dissolution = as.formula(paste("~", dissolution())),
+#                     targets = "formation",
+#                     offset.coef.diss = dissoffsets(),
+#                     estimate = estimate(),
+#                     control = stergmcontrols())
+#   }
+#
+#   return(fit)
+# })
 
 
 
