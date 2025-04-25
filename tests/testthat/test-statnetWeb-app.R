435b266c7976226bd10f560ce9db8d4bf0659ce0
Michał Bojanowski <michal2992@gmail.com>
2025-04-25 14:06:06 +0200

    Complete shinytest2 setup
    References #96

--- tests/testthat/test-statnetWeb-app.R
+++ tests/testthat/test-statnetWeb-app.R
@@ -2,8 +2,10 @@ library(shinytest2)
 
 test_that("statnetWeb app works", {
   # Don't run these tests on CRAN
-  skip_on_cran()
+  # skip_on_cran()
+  expect_true(TRUE)
   
-  app_path <- system.file(package = "statnetWeb", "statnetWeb")
-  test_app(app_path)
+  app_path <- "../../inst/shiny/statnetWeb"
+  # testthat::expect_identical(app_path, 1)
+  test_app(app_path, check_setup = TRUE)
 })
